6bf6bdf00683599fd10fd490546573970a5562dd
KauSantiagoPereira <kauan.santiago.pereira@escola.pr.gov.br>
2026-06-09 13:48:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 // Pegando os elementos do HTML
 const modal = document.getElementById("modalSustentabilidade");
-const btnSaibaMais = document.getElementById("btnSaibaMais");
-const botaoFechar = document.querySelector(".fechar");
+const btn = document.getElementById("btnSaibaMais");
+const spanFechar = document.getElementsByClassName("fechar")[0];
 
-// Função para abrir o modal (muda o display de 'none' para 'flex')
-btnSaibaMais.addEventListener("click", () => {
-    modal.style.display = "flex";
-});
+// Abre o modal ao clicar no botão
+btn.onclick = function() {
+    modal.style.display = "block";
+}
 
-// Função para fechar o modal ao clicar no (X)
-botaoFechar.addEventListener("click", () => {
+// Fecha o modal ao clicar no (X)
+spanFechar.onclick = function() {
     modal.style.display = "none";
-});
+}
 
-// Função para fechar o modal se o usuário clicar em qualquer lugar fora da caixinha branca
-window.addEventListener("click", (event) => {
-    if (event.target === modal) {
+// Fecha o modal se o usuário clicar em qualquer lugar fora dele
+window.onclick = function(event) {
+    if (event.target == modal) {
         modal.style.display = "none";
     }
-});
+}
